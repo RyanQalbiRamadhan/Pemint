466f60d0a16a58f3be5c8e0956d9600216803d14
RyanQalbiRamadhan <ryanqalbiramadhan@gmail.com>
2020-10-13 20:41:28 +0700

    Add store, destroy, and update for product

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ NIM  : 185150700111020
 
 #Tugas 1
 - Membuat sebuah route /biodata yang mana berisi data diri berupa nama dan NIM.
+
+#Tugas 2
+- Membuat sebuah tabel "migrations" dan "products" pada database lumen_pemint dengan menggunakan perintah artisan
+- Membuat beberapa route yaitu sebagai berikut
+  1.) $router->get('/products', 'ProductController@index');
+      Untuk menampilkan seluruh isi dari tabel.
+  2.) $router->get('/products/{id}', 'ProductController@show');
+      Untuk menampilkan data berdasarkan id.
+  3.) $router->post('/products', 'ProductController@store');
+      Untuk menambahkan data baru ke dalam tabel.
+  4.) $router->delete('/products/{id}', 'ProductController@destroy');
+      Untuk menghapus data dari tabel berdasarkan id.
+  5.) $router->put('/products/{id}', 'ProductController@update');
+      Untuk mengedit data berdasarkan id dari tabel.
--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,27 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+// Menampilkan data diri berupa nama dan NIM
 $router->get('/biodata', function (){
     return 'Nama : Ryan Gatutkaca Qalbi Ramadhan || NIM : 185150700111020';
 });
 
+// Menggenerate 32 alphanumeric secara random untuk app key
 $router->get('/key', function() {
     return \Illuminate\Support\Str::random(32);
 });
 
+// Menampilkan seluruh isi tabel
 $router->get('/products', 'ProductController@index');
+
+// Menampilkan data berdasarkan id dari tabel
 $router->get('/products/{id}', 'ProductController@show');
+
+// Menambahkan data baru ke dalam tabel
 $router->post('/products', 'ProductController@store');
+
+// Menghapus data berdasarkan id dari tabel
 $router->delete('/products/{id}', 'ProductController@destroy');
+
+// Mengupdate data berdasarkan id dari tabel
 $router->put('/products/{id}', 'ProductController@update');
