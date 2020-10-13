3d5e827360440d91f6bb5fe41025a5cbae5b1111
RyanQalbiRamadhan <ryanqalbiramadhan@gmail.com>
2020-10-13 19:07:36 +0700

    Add update for product

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,4 @@ $router->get('/products', 'ProductController@index');
 $router->get('/products/{id}', 'ProductController@show');
 $router->post('/products', 'ProductController@store');
 $router->delete('/products/{id}', 'ProductController@destroy');
+$router->put('/products/{id}', 'ProductController@update');
